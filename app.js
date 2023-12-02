b9e6062a653e9d48e9c509eea6a2871d887230f9
BNdungu <bryannganga909@gmail.com>
2023-12-02 10:26:09 +0300

    mpesa

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; // You can change the port as needed
+
+// Endpoint to handle Safaricom's callback URL
+app.post('/bar/callback', (req, res) => {
+  console.log('Received callback from Safaricom:', req.body); // Log the received data
+  res.status(200).send('Callback received successfully.'); // Send a response to Safaricom
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
