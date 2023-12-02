b924d3be6a2eb174b222694290a81e564c8dbea3
BNdungu <bryannganga909@gmail.com>
2023-12-02 11:22:13 +0300

    express.json()

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000; // You can change the port as needed
 
-// Endpoint to handle Safaricom's callback URL
+app.use(express.json())
 app.post('/', (req, res) => {
   console.log('Received callback from Safaricom:', req.body); // Log the received data
   res.status(200).send('Callback received successfully.'); // Send a response to Safaricom
